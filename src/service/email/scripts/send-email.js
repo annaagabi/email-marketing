5fba56fcf73557fe3282a5d3d8759b7804e7ba20
unknown <annaa0478@gmail.com>
2024-06-29 13:05:40 -0300

    Feat: send-email incompleto

--- src/service/email/scripts/send-email.js
+++ src/service/email/scripts/send-email.js
@@ -1,3 +1,13 @@
 emailjs.init({
     publicKey: "GTFgF8LiRkgI-YHA9"
 })
+
+function handleEmail(){
+    let templateParams = {
+        name: "Destinatário",
+        from_name: "Suporte de Domingo",
+        message: "Conteúdo da mensagem..."
+    }
+
+    emailjs.send("service_8dhuui9", )
+}
